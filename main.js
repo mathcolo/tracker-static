ca0fbf6ffb50c9046d88b58237247b58827dd61e
jjurman <jjurman@constantcontact.com>
2018-10-02 22:26:33 -0400

    update version of tram-one and webpack config to be like tram-one-express

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const Tram = require('tram-one')
+require("babel-polyfill")
 
-const app = new Tram()
+window.engine = {}
+const app = new Tram({
+  webEngine: window.engine
+})
 
 app.addRoute('/', require('./pages/home'))
 app.addRoute('/404', require('./pages/404'))
@@ -8,16 +12,5 @@ app.addRoute('/404', require('./pages/404'))
 app.addRoute('/privacy', require('./pages/privacy'))
 app.addRoute('/opensource', require('./pages/opensource'))
 app.addActions({ update: require('./actions/update')})
-
-window.engine = {
-    store: app.engine.store,
-    actions: app.engine.actions
-}
-app.addListener((store, actions) => {
-    // update the window store and actions
-    window.engine = {
-      store, actions
-    }
-  })
 
 app.start('.main')
