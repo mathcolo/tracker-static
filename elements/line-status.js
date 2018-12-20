31aef71fc00f0c1a91e9092603c86bbfb42a9ab2
Preston Mueller <skierjunk@gmail.com>
2018-12-19 21:33:14 -0700

    Update line box margin

--- elements/line-status.js
+++ elements/line-status.js
@@ -5,7 +5,7 @@ const html = Tram.html({
 
 module.exports = (attrs) => {  
   return html`
-    <div class="box" style="width: 100%; min-height: 300px; margin: 20px;${attrs.style || ''}">
+    <div class="box" style="width: 100%; min-height: 300px; margin: 15px 5px 15px 5px; ${attrs.style || ''}">
     <h4 class="subtitle is-4" style='color: ${attrs.color}'>${attrs.line} Line - ${attrs.description}</h4>
 
       <line-status-line line='${attrs.line}' mode='std' count=14 model=1800 />
