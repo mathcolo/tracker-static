7f013d216e939cb88f9ccf8c36c7aa57b9e638a0
Preston Mueller <skierjunk@gmail.com>
2019-01-29 21:37:54 -0500

    Use status2 route instead

--- actions/update.js
+++ actions/update.js
@@ -1,7 +1,7 @@
 module.exports = {
   init: () => {},
   tick: (store, _, actions) => {
-    fetch('/puller/status').then((resp) => resp.json())
+    fetch('/puller/status2').then((resp) => resp.json())
       .then((resp) => {
         actions.setData(resp);
       });
